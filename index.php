ddb3d576f4b1e4f922f28fdcb42eb0bc4d4de732
Cody Wilson <cody@codywilson.co>
2013-10-09 21:12:54 -0500

    add jquery

--- index.php
+++ index.php
@@ -136,6 +136,8 @@ function register($mac) {
 <html class="notie67 wf-yanonekaffeesatz-n4-active wf-yanonekaffeesatz-n2-active wf-active">
 <head>
 <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+<script type='text/javascript'>try{jQuery.noConflict();}catch(e){};</script>
 <script type='text/javascript' src='http://www.qccolab.com/wp-content/uploads/montezuma/javascript.js?ver=3.6.1'></script>
 </head>
 <body>
